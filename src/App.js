ada93b81f708d598e6ed5b7e791ce2cd4a82a217
Хворых Виталий <vithvo@bk.ru>
2022-08-23 14:41:59 +0700

    added starting components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import Categories from "./components/Categories";
+import Header from "./components/Header";
+import Sort from "./components/Sort";
+import Block from "./components/PizzaBlock";
+
+import "./scss/app.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            <Block
+              title="Мексиканская"
+              price={500}
+              imgUrl="./img/pizza/1.png"
+            />
+            <Block
+              title="Перуанская"
+              price={450}
+              imgUrl="./img/pizza/2.png"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
